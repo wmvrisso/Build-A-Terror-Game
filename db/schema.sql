a6bb020ed8c193aac77a36cad550d1ad6d1ed6d6
bret jensen <jensenbret13@gmail.com>
2025-03-03 19:36:16 -0800

    .

--- db/schema.sql
+++ db/schema.sql
@@ -4,38 +4,43 @@ CREATE DATABASE creation_db;
 /c creation_db;
 
 CREATE TABLE monster (
-    head
-    torso
-    arm1
-    arm2
-    leg1
-    leg2
-    strength
+    monst_name VARCHAR(30),
+    head,
+    torso,
+    arm1,
+    arm2,
+    leg1,
+    leg2,
+    strength VARCHAR(30)
 -- will these all need to be foregin keys? Would an ID or name be needed?
 );
-
 CREATE TABLE arm1 (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    arm1_name VARCHAR(30)
 );
 -- Is there a way to combine arm1 and 2 into the same table? Or rather, a user would just choose 'Arms', and then get two selections?
 CREATE TABLE arm2 (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    arm2_name VARCHAR(30)
 );
-
 CREATE TABLE leg1 (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    leg1_name VARCHAR(30)
 );
-
+-- same question for the legs as the arms?
 CREATE TABLE leg2 (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    leg2_name VARCHAR(30)
 );
-
 CREATE TABLE head (
     id SERIAL PRIMARY KEY
-)
+    head_name VARCHAR(30)
+);
 CREATE TABLE torso (
-    id SERIAL PRIMARY KEY
-)
+    id SERIAL PRIMARY KEY,
+    torso_name VARCHAR(30)
+);
 CREATE TABLE strength (
     id SERIAL PRIMARY KEY
-)
+    strength_name VARCHAR(30)
+);
