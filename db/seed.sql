1824da3a3fd29405401a9a63d44575387f0f1a79
bret jensen <jensenbret13@gmail.com>
2025-03-03 20:57:44 -0800

    .

--- db/seed.sql
+++ db/seed.sql
@@ -16,9 +16,45 @@ INSERT INTO monst_type (monst_type_name)
     VALUES
     ('Beast'),
     ('Mythic'),
-    ('UNdead'),
+    ('Undead'),
     ('Draconic'),
     ('Phantom'),
     ('Golem'),
 
-INSERT INTO 
+INSERT INTO strength (strength_name)
+    VALUES
+    ('firebreath'),
+    ('plasma ray'),
+    ('invisibility'),
+-- think of more
+
+INSERT INTO arm1 (arm1_name)
+    VALUES  
+    ('chainsaw'),
+    ('dragon claw'),
+    ('bear  claw'),
+    ('tree branch'),
+    ('plasma gun'),
+    -- add more
+
+INSERT INTO arm2 (arm2_name)
+    VALUES  
+    ('chainsaw'),
+    ('dragon claw'),
+    ('bear  claw'),
+    ('tree branch'),
+    ('plasma gun'),
+
+INSERT INTO leg1 (leg1_name)
+    VALUES
+    ('tree branch'),
+    ('')
+
+INSERT INTO leg2 (leg2_name)
+    VALUES
+    ('tree branch'),
+    ('')
+
+INSERT INTO torso (torso_name)
+    VALUES 
+    -- add values
