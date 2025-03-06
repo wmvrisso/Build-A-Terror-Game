d80c8728bc021c109338120c3edd5dfb7bcdaca8
Tyler Hayman <tyler.hayman@icloud.com>
2025-03-05 22:36:08 -0700

    changes to seql to include stats

--- db/seed.sql
+++ db/seed.sql
@@ -1,60 +1,65 @@
-INSERT INTO head (head_name)
-    VALUES
-    ('Dragon'),
-    ('Dragon'),
-    ('Dragon'),
-    ('Dragon'),
-    ('Dragon'),
-    ('Dragon'),
-    ('Dragon'),
-    ('Dragon'),
-    ('Dragon'),
-    ('Dragon'),
-    ('Dragon'),
+ALTER TABLE head ADD COLUMN speed INT, ADD COLUMN health INT, ADD COLUMN damage INT;
+ALTER TABLE monst_type ADD COLUMN speed INT, ADD COLUMN health INT, ADD COLUMN damage INT;
+ALTER TABLE strength ADD COLUMN speed INT, ADD COLUMN health INT, ADD COLUMN damage INT;
+ALTER TABLE arm1 ADD COLUMN speed INT, ADD COLUMN health INT, ADD COLUMN damage INT;
+ALTER TABLE arm2 ADD COLUMN speed INT, ADD COLUMN health INT, ADD COLUMN damage INT;
+ALTER TABLE leg1 ADD COLUMN speed INT, ADD COLUMN health INT, ADD COLUMN damage INT;
+ALTER TABLE leg2 ADD COLUMN speed INT, ADD COLUMN health INT, ADD COLUMN damage INT;
+ALTER TABLE torso ADD COLUMN speed INT, ADD COLUMN health INT, ADD COLUMN damage INT;
 
-INSERT INTO monst_type (monst_type_name)
+INSERT INTO head (head_name, speed, health, damage)
     VALUES
-    ('Beast'),
-    ('Mythic'),
-    ('Undead'),
-    ('Draconic'),
-    ('Phantom'),
-    ('Golem'),
+    ('Dragon', 10, 100, 50),
+    ('Undead', 10, 100, 50),
+    ('Mythical', 10, 100, 50),
+    ('Phantom', 10, 100, 50),
+    ('Beast', 10, 100, 50),
+    ('Golem', 10, 100, 50),
 
-INSERT INTO strength (strength_name)
+INSERT INTO monst_type (monst_type_name, speed, health, damage)
+    VALUES
+    ('Beast', 20, 150, 30),
+    ('Mythic', 15, 200, 40),
+    ('Undead', 5, 250, 20),
+    ('Draconic', 10, 300, 60),
+    ('Phantom', 25, 100, 50),
+    ('Golem', 5, 400, 70),
+
+INSERT INTO strength (strength_name, speed, health, damage)
     VALUES
     ('firebreath'),
     ('plasma ray'),
     ('invisibility'),
 -- think of more
 
-INSERT INTO arm1 (arm1_name)
-    VALUES  
-    ('chainsaw'),
-    ('dragon claw'),
-    ('bear  claw'),
-    ('tree branch'),
-    ('plasma gun'),
+INSERT INTO arm1 (arm1_name, speed, health, damage)
+    VALUES
+    ('chainsaw', 5, 50, 80),
+    ('dragon claw', 10, 100, 90),
+    ('bear  claw', 8, 80, 70),
+    ('tree branch', 3, 30, 40),
+    ('plasma gun', 7, 70, 100),
     -- add more
 
-INSERT INTO arm2 (arm2_name)
-    VALUES  
-    ('chainsaw'),
-    ('dragon claw'),
-    ('bear  claw'),
-    ('tree branch'),
-    ('plasma gun'),
-
-INSERT INTO leg1 (leg1_name)
+INSERT INTO arm2 (arm2_name, speed, health, damage)
     VALUES
-    ('tree branch'),
-    ('tank track')
+    ('chainsaw', 5, 50, 80),
+    ('dragon claw', 10, 100, 90),
+    ('bear  claw', 8, 80, 70),
+    ('tree branch', 3, 30, 40),
+    ('plasma gun', 7, 70, 100),
 
-INSERT INTO leg2 (leg2_name)
+INSERT INTO leg1 (leg1_name, speed, health, damage)
     VALUES
-    ('tree branch'),
-    ('tank track')
+    ('tree branch', 3, 30, 10),
+    ('tank track', 2, 200, 20)
 
-INSERT INTO torso (torso_name)
-    VALUES 
-    -- add values
+INSERT INTO leg2 (leg2_name, speed, health, damage)
+    VALUES
+    ('tree branch', 3, 30, 10),
+    ('tank track', 2, 200, 20)
+
+INSERT INTO torso (torso_name, speed, health, damage)
+    VALUES
+    ('steel armor', 0, 500, 0),
+    ('dragon scales', 0, 400, 0);
