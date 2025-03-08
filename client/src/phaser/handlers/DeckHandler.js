fea45c8b41b8e30a652cdb11d15e32667e22a129
4ccraymond <4ccraymond@gmail.com>
2025-03-08 07:27:52 -0700

    deck handler adjustments

--- client/src/phaser/handlers/DeckHandler.js
+++ client/src/phaser/handlers/DeckHandler.js
@@ -1,21 +1,5 @@
 import api from "../../services/api";
 
-
-export default class DeckHandler {
-  constructor(scene) {
-    this.scene = scene;
-    this.deck = []; // Main deck of cards
-  }
-
-//  Loads deck and shuffles
-  async loadDeck() {
-    try {
-      const response = await api.get("/cards"); // Fetch all cards
-      this.deck = this.shuffleDeck(response.data);
-    } catch (error) {
-      console.error("Error fetching deck:", error);
-
-
 export default class DeckHandler {
   constructor(scene) {
     this.scene = scene;
@@ -77,77 +61,35 @@ export default class DeckHandler {
       default:
         console.warn("Invalid deck type!");
         return [];
-
     }
-  }
-
-
-// Shuffles deck
-  shuffleDeck(deck) {
-    for (let i = deck.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [deck[i], deck[j]] = [deck[j], deck[i]];
 
     if (deck.length < num) {
       console.warn(`Not enough cards in the ${deckType} deck!`);
       return [];
     }
-    return deck;
-  }
-
-//   Draws a number of cards from the deck
-  drawCards(num) {
-    if (this.deck.length < num) {
-      console.warn("Not enough cards in the deck!");
-      return [];
-    }
-
-    return this.deck.splice(0, num); // Remove and return drawn cards
-  }
-
-// Counts remaining cards in the deck
-  getRemainingCards() {
-    return this.deck.length;
-  }
-}
-
     return deck.splice(0, num);
   }
-<<<<<<< HEAD
-=======
 
-// Counts remaining cards in the deck
+  // Counts remaining cards in the deck
   getRemainingCards(deckType) {
     switch (deckType) {
-      case 'head':
+      case 'Head':
         return this.headDeck.length;
-      case 'torso':
-        return this.torsoDeck.length;
-      case 'legs':
+      case 'Body':
+        return this.bodyDeck.length;
+      case 'Legs':
         return this.legsDeck.length;
       default:
         console.warn("Invalid deck type!");
         return 0;
     }
   }
-    constructor(){
-      this.deck = [];
+
+  getDeck() {
+    return [...this.headDeck, ...this.bodyDeck, ...this.legsDeck];
   }
 
-  addCard(card){
-      this.deck.push(card);
+  getCardById(id) {
+    return this.getDeck().find(c => c.id === id);
   }
-
-  removeCard(card){
-      this.deck = this.deck.filter(c => c.id !== card.id);
-  }
-
-  getDeck(){
-      return this.deck;
-  }
-
-  getCardById(id){
-      return this.deck.find(c => c.id === id);
-  }
->>>>>>> 00f9df51bb6bfa962fb440b6b2f3fc51903229c4
 }
