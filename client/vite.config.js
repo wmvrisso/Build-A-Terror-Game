21307a1717695092c02dcea4dd836805a497ce31
wmvrisso <wmvrisso@gmail.com>
2025-03-09 18:18:36 -0700

    added App.css, removed tailwind  config.js files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,13 @@
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss"; //
+import autoprefixer from "autoprefixer"; //
 
-export default {
+export default defineConfig({
   plugins: [react()],
-};
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+});
