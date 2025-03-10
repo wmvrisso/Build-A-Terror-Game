293dd49aca5e5534cbd4b937570becf6d48a4426
bret jensen <jensenbret13@gmail.com>
2025-03-09 19:18:20 -0700

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 // import Header from './components/Header'
 // import Footer from './components/Footer'
 import GameUI from "./gameUI.jsx";
-import "./input.css";
+//import "./App.css";  TODO: uncomment once App.css file is pushed to directory
 
 function App() {
   return (
