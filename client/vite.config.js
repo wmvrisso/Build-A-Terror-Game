0e99dc7ae1bc58cbd30a75e543241981b3d6db38
4ccraymond <4ccraymond@gmail.com>
2025-03-10 09:35:51 -0600

    double checking my pull worked before, need to commit first.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss"; //
-import autoprefixer from "autoprefixer"; //
+// import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss(), autoprefixer()],
-    },
-  },
+  // css: {
+  //   postcss: {
+  //     plugins: [tailwindcss(), autoprefixer()],
+  //   },
+  // },
 });
