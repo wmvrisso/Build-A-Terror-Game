d4527d35732de97c198b7683b6570f6a574dc86c
bret jensen <jensenbret13@gmail.com>
2025-03-10 21:02:18 -0700

    app.jsx again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GameUI from "./gameUI.jsx";
 import Header from "./components/Header"; // Uncomment if needed
 import Footer from "./components/Footer"; // Uncomment if needed
- 
-// import Left from "./components/Left";
+
 
 function App() {
   return (
     <Router>
       <Header />
-      <div>
-        {/* Left section added here */}
-        {/* <Left /> */}
-
-        {/* Right section (Routes) */}
-        <div>
-          <Routes>
-            <Route path="/" element={<GameUI />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<GameUI />} />
+      </Routes>
       <Footer />
     </Router>
   );
