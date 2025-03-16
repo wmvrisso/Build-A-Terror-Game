921f3d4337f6f3930e11ae7e3df5147b7b0c7450
wmvrisso <wmvrisso@gmail.com>
2025-03-16 13:14:33 -0700

    added .css imports to files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Phaser from "phaser";
 import PhaserGame from "./game/PhaserGame";
+import "./public/style.css";
 
 // This is the main entry point for the React application. It imports the PhaserGame component and sets up the main game UI.
 function App() {
