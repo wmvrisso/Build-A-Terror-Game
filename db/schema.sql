3652c38f175cb46fd743121f65c048ae94afceab
wmvrisso <wmvrisso@gmail.com>
2025-03-18 21:50:01 -0700

    resolved conflicts again

--- db/schema.sql
+++ db/schema.sql
@@ -34,10 +34,6 @@ CREATE TABLE effectiveness_chart (
     id SERIAL PRIMARY KEY,
     attacker VARCHAR(50) REFERENCES monster_types(name),
     defender VARCHAR(50) REFERENCES monster_types(name),
-<<<<<<< HEAD
-    multiplier FLOAT NOT NULL
-);
-=======
     multiplier DOUBLE PRECISION NOT NULL
 );
 
@@ -49,4 +45,3 @@ CREATE TABLE Users (
     createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
->>>>>>> 2ae6fd15f58bba88408422c52ec9fb5ff4156f7a
